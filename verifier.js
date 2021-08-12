c6c076937858ab1559d87da29cf5635df15ca029
k-mill1 <kaisamill@gmail.com>
2021-08-12 16:12:22 +0100

    clean up code

--- verifier.js
+++ verifier.js
@@ -1,24 +1,24 @@
 function verifyLive(input) {
    
     // live cell with fewer than two live neighbours dies as if by under population
-    if(input.neighbours < 2){
+    if(input.neighbours < 2) {
         return false;
     } 
     
     // live cell with more than three live neighbours dies, as if by overcrowding
-    if(input.neighbours > 3){
+    if(input.neighbours > 3) {
         return false;
     }
     
     // live cell with two or three live neighbours lives on to the next generation
-    if(input.neighbours === 2 ){
+    if(input.neighbours === 2 || input.neighbours === 3) {
         return true;
     }   
 
     // dead cell with exactly three live neighbours becomes a live cell
-    if(input.neighbours === 3 && input.status === true){
+    if(input.neighbours === 3) {
         return true;
     } 
 }
   
-  export { verifyLive };
+export { verifyLive };
--- verifylive.test.js
+++ verifylive.test.js
@@ -1,19 +1,19 @@
 import { verifyLive } from "./verifier";
 
 const cell = {
-  neighbours: 1,
+  neighbours: 1
 };
 const cell2 = {
-  neighbours: 4,
+  neighbours: 4
 };
 const cell3 = {
-  neighbours: 2,
+  neighbours: 2
 };
 const cell4 = {
-  neighbours: 3,
+  neighbours: 3
 };
 const cell5 = {
-  neighbours: 3,
+  neighbours: 3
 };
 
 describe('Neighbours', () => {
@@ -23,23 +23,18 @@ describe('Neighbours', () => {
   });
   test("more than three neighbours, dies of overcrowding", () => {
     const result = verifyLive(cell2);
-    expect(result).toBe(false);
-    
-});
+    expect(result).toBe(false); 
+  });
   test("cell with two live neighbours lives on to the next generation.", () => {
     const result = verifyLive(cell3);
     expect(result).toBe(true);
-  
-});
-test("cell with three live neighbours lives on to the next generation.", () => {
-  const result = verifyLive(cell4);
-  expect(result).toBe(true);
-
-});
-test("cell with three live neighbours lives on to the next generation.", () => {
-  const result = verifyLive(cell5);
-  expect(result).toBe(true);
-
-});
-
+  });
+  test("cell with three live neighbours lives on to the next generation.", () => {
+    const result = verifyLive(cell4);
+    expect(result).toBe(true);
+  });
+  test("cell with three live neighbours lives on to the next generation.", () => {
+    const result = verifyLive(cell5);
+    expect(result).toBe(true);
+  });
 });
